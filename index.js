078b8711dbed77a3384bb9634b60e422fb49d4d4
theopoop <109771847+theopoop@users.noreply.github.com>
2022-07-21 17:35:31 -0700

    Add files via upload
    i add

---
+++ index.js
@@ -0,0 +1,17 @@
+let count = 0
+
+
+function katzeCount() {
+    count += 1;
+    document.getElementById("marioracer").innerText = "Number of times i baet quaffed mario carter: " + count;
+    console.log("kill yourself");
+}
+
+
+
+
+
+
+
+
+
