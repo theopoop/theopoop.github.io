b3191f278b6815dae6d0d75134e91eb97b990409
theopoop <109771847+theopoop@users.noreply.github.com>
2022-07-21 17:48:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let count = 0
 function marioWin() {
     count += 1;
     document.getElementById("marioracer").innerText = "Number of times i beat quaffed mario carter: " + count;
+    alert("quadded ==== POOOOOOP💩💩💩");
 }
 
 
