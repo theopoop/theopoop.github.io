0bb75273c5c01193beb41bfb861280c0ddd68b05
theopoop <109771847+theopoop@users.noreply.github.com>
2022-07-21 17:37:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let count = 0
 
 function katzeCount() {
     count += 1;
-    document.getElementById("marioracer").innerText = "Number of times i baet quaffed mario carter: " + count;
+    document.getElementById("marioracer").innerText = "Number of times i beat quaffed mario carter: " + count;
     console.log("kill yourself");
 }
 
